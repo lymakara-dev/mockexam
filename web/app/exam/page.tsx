0dae9df6af47e604571786ad538de3b1cca6e814
lymakara-dev <ly.makara.personal@gmail.com>
2024-10-27 15:46:46 +0700

    build exam card wth props

--- web/app/exam/page.tsx
+++ web/app/exam/page.tsx
@@ -1,10 +1,71 @@
 "use client";
 
 import React from "react";
-import ClassCard from "@/components/card";
+import ExamCard from "@/components/card";
 import { Image } from "@nextui-org/image";
 
 function ExamPage() {
+  const list = [
+    {
+      title: "ការប្រលងគណិតគណនា",
+      img: "/images/cal.png",
+      price: "$5.50",
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      date: new Date().toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }),
+    },
+
+    {
+      title: "ការប្រលងតក្កវិទ្យា",
+      img: "/images/logic.png",
+      price: "$5.50",
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      date: new Date().toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }),
+    },
+
+    {
+      title: "ការប្រលងរូបវិទ្យា",
+      img: "/images/physic.png",
+      price: "$5.50",
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      date: new Date().toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }),
+    },
+
+    {
+      title: "ការប្រលងគីមីវិទ្យា",
+      img: "/images/chemistry.png",
+      price: "$5.50",
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      date: new Date().toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }),
+    },
+  ];
   return (
     <div className="flex flex-col gap-5">
       <div className="flex flex-row gap-2">
@@ -29,7 +90,18 @@ function ExamPage() {
       <p className="text-2xl not-italic font-medium text-[#64748B] mt-5">
         ថ្នាក់ប្រលង
       </p>
-      <ClassCard />
+
+      <div className="flex flex-col gap-4 md:flex-row items-center flex-wrap">
+        {list.map((item, index) => (
+          <ExamCard
+            key={index}
+            title={item.title}
+            img={item.img}
+            time={item.time}
+            date={item.date}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- web/components/card.tsx
+++ web/components/card.tsx
@@ -1,106 +1,46 @@
 import React from "react";
 import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
 
-function ClassCard() {
-  const list = [
-    {
-      title: "ការប្រលងគណិតគណនា",
-      img: "/images/cal.png",
-      price: "$5.50",
-      time: new Date().toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-      date: new Date().toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }),
-    },
-
-    {
-      title: "ការប្រលងតក្កវិទ្យា",
-      img: "/images/logic.png",
-      price: "$5.50",
-      time: new Date().toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-      date: new Date().toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }),
-    },
-
-    {
-      title: "ការប្រលងរូបវិទ្យា",
-      img: "/images/physic.png",
-      price: "$5.50",
-      time: new Date().toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-      date: new Date().toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }),
-    },
-
-    {
-      title: "ការប្រលងគីមីវិទ្យា",
-      img: "/images/chemistry.png",
-      price: "$5.50",
-      time: new Date().toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-      }),
-      date: new Date().toLocaleDateString("en-US", {
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      }),
-    },
-  ];
-  return (
-    <div className="flex flex-row gap-4">
-      {list.map((item, index) => (
-        <Card
-          shadow="sm"
-          key={index}
-          isPressable
-          onPress={() => console.log("item pressed")}
-          className="w-[185px] h-[279px] p-[12px] flex flex-col items-cente"
-        >
-          <CardBody className="grow-0">
-            <Image
-              radius="none"
-              width="100%"
-              alt={item.title}
-              className="w-[115px] h-[120px]"
-              src={item.img}
-            />
-          </CardBody>
-          <CardFooter className="grow flex flex-col p-0 pt-[10px] items-start">
-            <p className="grow text-[16px] font-normal​​ font-medium">
-              {item.title}
-            </p>
-            <div className="grow-0">
-              <div className="flex flex-row gap-1">
-                <Image src="/svg/clock.svg" />
-                <p className="text-default-500">{item.time}</p>
-              </div>
-              <div className="flex flex-row gap-1">
-                <Image src="/svg/calendar-date-range.svg" />
-                <p className="text-default-500">{item.date}</p>
-              </div>
-            </div>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
-  );
+// Define the prop types
+interface ExamCardProps {
+  title: string;
+  img: string;
+  time: string;
+  date: string;
 }
 
-export default ClassCard;
+const ExamCard: React.FC<ExamCardProps> = ({ title, img, time, date }) => {
+  return (
+    <Card
+      shadow="sm"
+      isPressable
+      onPress={() => console.log("item pressed")}
+      className="w-[185px] h-[279px] p-[12px] flex flex-col items-cente"
+    >
+      <CardBody className="grow-0">
+        <Image
+          radius="none"
+          width="100%"
+          alt={title}
+          className="w-[115px] h-[120px]"
+          src={img}
+        />
+      </CardBody>
+      <CardFooter className="grow flex flex-col p-0 pt-[10px] items-start">
+        <p className="grow text-[16px] font-normal​​ font-medium">{title}</p>
+        <div className="grow-0">
+          <div className="flex flex-row gap-1">
+            <Image src="/svg/clock.svg" />
+            <p className="text-default-500">{time}</p>
+          </div>
+          <div className="flex flex-row gap-1">
+            <Image src="/svg/calendar-date-range.svg" />
+            <p className="text-default-500">{date}</p>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ExamCard;
