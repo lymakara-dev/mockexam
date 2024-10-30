f7504169166cebfcd33ba3b715f2b6120dc5a51b
lengkimtry <kimt47494@gmail.com>
2024-10-30 13:58:45 +0700

    First

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <h1>You are login with cookies set to 2mn and all routes are protected with middleware </h1>
-  )
-}
-
-export default page
