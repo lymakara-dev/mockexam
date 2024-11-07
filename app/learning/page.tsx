37d713f8bad9dc8d65499eec9edef0c59bd940cd
Your Name <you@example.com>
2024-11-07 15:19:27 +0700

    Close themeswitch

---
+++ app/learning/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <img src="https://techbox.developimpact.net/documents/1765848/1765850/A2.png/f0767fc4-05de-72c0-fcf6-7e3d322ec0f1?version=1.0&t=1730446617043&download=true&objectDefinitionExternalReferenceCode=eadd7ee4-55cf-14ea-67b1-ead0d3dfdf3e&objectEntryExternalReferenceCode=1fb53f37-b401-fa57-5544-1ccdc12be01a" alt="No" />
+    </div>
+  )
+}
+
+export default page
--- app/mockexam/[slug]/Questions.tsx
+++ app/mockexam/[slug]/Questions.tsx
@@ -144,7 +144,6 @@ fetch(url, {
        },
         body: JSON.stringify(jsonObject)
   })
-	//  .then(url => url.json())
   .then(url => {
     console.log("Record created successfully!", url)
     // setCheck(false);
@@ -208,10 +207,13 @@ fetch(url, {
     
     const handleChange = (value: string) => {
       setSelectedValue(value);
-      const checkBox = parseInt(value);      
+      const checkBox = parseInt(value);    
+      console.log(mathItems[index]);
+        
           
       if (checkBox === mathItems[index].correctedAns) {
         checkScore = true;
+
             
       }else{
         checkScore = false;        
@@ -309,17 +311,8 @@ fetch(url, {
               style={{ width: "100%", height: "100" }}
             />
           </div>: "Content not found"}
-          {mathItems[0]?.answer?.link?.href ?
-            <img
-            alt="Tests Question"
-            height={0}
-            sizes="100vw"
-            src={url+mathItems[index].answer.link.href}
-            style={{ width: "100%", height: "100" }}
-            width={0}
-            />
-            : ""
-          }
+            <img src="https://techbox.developimpact.net/documents/1765848/1765850/A2.png/f0767fc4-05de-72c0-fcf6-7e3d322ec0f1?version=1.0&t=1730446617043&download=true&objectDefinitionExternalReferenceCode=eadd7ee4-55cf-14ea-67b1-ead0d3dfdf3e&objectEntryExternalReferenceCode=1fb53f37-b401-fa57-5544-1ccdc12be01a" alt="Test" />
+          
           <h1 className="font-bold text-2xl text-left mt-2">ចម្លើយ</h1>
           <span className="bg-gray-500 h-[1px] w-full max-w-[500px] ml-4 mt-4 sm:block" />
           <div className="multiplechoice">
