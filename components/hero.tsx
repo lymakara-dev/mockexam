5106fe9c0eb504ffa9d4239b976bcc94a692df32
Bath <khemsombath123@gmail.com>
2024-11-12 07:37:42 +0000

    Update

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,33 +9,34 @@ function Hero() {
         <Image
           className="absolute h-full w-full inset-0 object-cover"
           src="/images/herobackground.jpg"
-          width={100}
-          height={100}
+          width={2000}
+          height={2000}
           alt="img"
         />
       </div>
 
       {/* gradient layer */}
-      <div className="absolute bg-gradient-to-t from-[#254961] to-[#00000] border-larg  top-0 left-0 w-full h-full text-black"></div>
 
       {/* text layer */}
-      <div className="absolute w-full h-full">
-        <div className="flex flex-col items-center">
-          <p>សាកល្បង</p>
-          <div className="flex flex-col">
-            <h1 className="text-[48px]">ប្រលងចូលតិចណូ</h1>
-            <p className="self-end">និងរៀនត្រៀមដោយសេរី...!</p>
+      <div className=" w-full h-full flex flex-col  relative">
+        <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white">
+          <div className="flex flex-col items-center">
+            <p>សាកល្បង</p>
+            <div className="flex flex-col">
+              <h1 className="text-[48px]">ប្រលងចូលតិចណូ</h1>
+              <p className="self-end">និងរៀនត្រៀមដោយសេរី...!</p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-4 bg-red-400 items-center">
-          <p className="text-center">
-            ប្រព័ន្ធប្រលងសាកល្បង ជាគេហទំព័រជំនួយដល់សិស្សានុសិស្សដែលមានបំណង​{" "}
-            <br />
-            ​ត្រៀមប្រឡងចូលវិទ្យាស្ថានបច្ចេកវិទ្យាកម្ពុជា។
-          </p>
-          <button className="h-[40px] w-[184px] text-[16px] bg-[#254061] text-white rounded-2xl ">
-            សាកល្បងឥឡូវនេះ​ &gt;
-          </button>
+          <div className="flex flex-col gap-4  items-center">
+            <p className="text-center">
+              ប្រព័ន្ធប្រលងសាកល្បង ជាគេហទំព័រជំនួយដល់សិស្សានុសិស្សដែលមានបំណង​{" "}
+              <br />
+              ​ត្រៀមប្រឡងចូលវិទ្យាស្ថានបច្ចេកវិទ្យាកម្ពុជា។
+            </p>
+            <button className="h-[40px] w-[184px] text-[16px] bg-common-blue text-white rounded-full hover:border-white hover:border-2 hover:bg-transparent  ">
+              សាកល្បងឥឡូវនេះ​ &gt;
+            </button>
+          </div>
         </div>
       </div>
     </div>
