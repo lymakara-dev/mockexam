834d771bd50bb13acc77b77e58491e9a8c75c1a6
Your Name <you@example.com>
2025-05-16 21:54:31 +0700

    Fix dynamic slug route

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { getTranslations } from 'next-intl/server';
 import React from 'react';
 import KhmerWebsitePage from "@/components/KhmerWebsitePage";
-
+export const runtime = 'edge';
 export default async function HomePage() {
   const t = await getTranslations("Footer");
   return (
